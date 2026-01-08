2f9cb63b6596cee586ae2a791197c0d29a1bba81
ogbueze <ogbuezeokenyi@gmail.com>
2026-01-08 22:17:19 +0100

    add components header and footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'
+import Header from './components/Header'
+
 
 function App() {
 
   return (
     <>
     <WelcomeMessage />
-      
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
