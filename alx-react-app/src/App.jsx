644760442433b570ddbe7c34858953f8b18e45f6
ogbueze <ogbuezeokenyi@gmail.com>
2026-01-08 22:03:35 +0100

    debug

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import WelcomeMessage from './WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
 
