a039eb6f18331cf48f788262ba35bd7aacc10a04
ogbueze <ogbuezeokenyi@gmail.com>
2026-01-08 22:22:05 +0100

    add props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
 import Header from './components/Header'
+import UserProfile from './components/UserProfile'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   )
